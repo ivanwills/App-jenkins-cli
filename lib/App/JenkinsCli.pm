14af7e7372bcb456bc30bfe343d6332871ebbdad
Ivan Wills <Ivan.Wills2@optus.com.au>
2017-02-05 15:27:19 +1100

    Updating for release of version 0.005

--- lib/App/JenkinsCli.pm
+++ lib/App/JenkinsCli.pm
@@ -16,7 +16,7 @@ use Term::ANSIColor qw/colored/;
 use File::ShareDir qw/dist_dir/;
 use Path::Tiny;
 
-our $VERSION = "0.004";
+our $VERSION = "0.005";
 
 has [qw/base_url api_key api_pass test/] => (
     is => 'rw',
@@ -331,7 +331,7 @@ App::JenkinsCli - Command line tool for interacting with Jenkins
 
 =head1 VERSION
 
-This documentation refers to App::JenkinsCli version 0.004
+This documentation refers to App::JenkinsCli version 0.005
 
 =head1 SYNOPSIS
 
